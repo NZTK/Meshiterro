fa6ee1d40ba9b187c2f26c704c95c229f7f5798d
NZTK <tkm.2438nishizawa@gmail.com>
2019-05-12 18:29:07 +0900

    [Add]詳細ページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '08505451fe76059b24c97c803dfac31e76af1f86a2dcbe8e7d333d3987c8a1aa3d595bb3da44f9df686f06670e14492c7dcd53be77c4bdccfc442d42e18ef3d2'
